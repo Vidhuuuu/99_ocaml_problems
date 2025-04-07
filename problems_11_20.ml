384f1230e1ee49436e8b022259241b36386c4e13
vidhu <vidhudash@gmail.com>
2025-04-08 00:55:15 +0530

    feat: done till 18

--- problems_11_20.ml
+++ problems_11_20.ml
@@ -32,9 +32,82 @@ let decode lst =
   List.rev (aux [] lst)
 ;;
 
+(* 13 - RLE direct *)
+(* same as 11? *)
+
+(* 14 - duplicate elems of a list *)
+let duplicate lst =
+  let rec aux acc = function
+    | [] -> acc
+    | hd :: tl -> aux (hd :: hd :: acc) tl
+  in
+  List.rev (aux [] lst)
+;;
+
+(* 15 - replicate elems n times *)
+let replicate lst n =
+  let rec aux acc = function
+    | [] -> acc
+    | hd :: tl -> aux (nadd_to acc n hd) tl
+  in
+  List.rev (aux [] lst)
+;;
+
+(* 16 - drop nth *)
+let drop lst n =
+  let rec aux acc curr = function
+    | [] -> acc
+    | hd :: tl ->
+        if curr = 1 then aux acc n tl
+        else aux (hd :: acc) (curr - 1) tl
+  in
+  List.rev (aux [] n lst)
+;;
+
+(* 17 - split two lists *)
+let split lst n =
+  let rec aux acc i = function
+    | [] -> (List.rev acc, [])
+    | hd :: tl as li ->
+        if i = 0 then (List.rev acc, li)
+        else aux (hd :: acc) (i - 1) tl
+  in
+  aux [] n lst
+;;
+
+(* 18 - extract slice *)
+let slice lst i k =
+  let rec aux acc curr = function
+    | [] -> acc
+    | hd :: tl ->
+        if curr >= i && curr <= k then aux (hd :: acc) (curr + 1) tl
+        else aux acc (curr + 1) tl
+  in
+  List.rev (aux [] 0 lst)
+;;
+
+
 let lst = ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"]
 let lst2 = [Many (4,"a"); One "b"; Many (2,"c"); Many (2,"a"); One "d"; Many (4,"e")]
+let lst3 = ["a";"b";"c";"c";"d"]
+let lst4 = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"]
+
+let print_list f lst =
+  print_string "[";
+  let rec aux = function
+    | [] -> ()
+    | [x] -> f x
+    | x :: xs ->
+        f x;
+        print_string "; ";
+        aux xs
+  in
+  aux lst;
+  print_endline "]"
+;;
 
 let () =
-  List.iter (fun x -> Printf.printf "%s " x) (decode lst2);
+  List.iter (fun x -> Printf.printf "%s " x) (slice lst4 2 6);
   print_newline ()
+  (* match (split lst4 4) with *)
+  (* | (x, y) -> print_list print_string x; print_list print_string y *)
